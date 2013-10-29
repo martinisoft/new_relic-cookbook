f5ba5a954569c3964f6628134b73ab44600dfecf
Aaron Kalin <akalin@martinisoftware.com>
2013-10-29 02:18:17 -0500

    Update version and description

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name             "new_relic"
 maintainer       "Aaron Kalin"
 maintainer_email "aaron@teamtreehouse.com"
 license          "Apache 2.0"
-description      "Installs and configures the New Relic agents for Treehouse"
+description      "Installs and configures the New Relic Agents"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.1.0"
+version          "1.0.0"
 
 supports "ubuntu"
 
